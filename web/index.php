9a615c91203ff98681c18deb4b5d78e8b9ce05a7
dman <dman@coders.co.nz>
2025-12-25 15:38:25 +0700

    placeholder front page & css

---
+++ web/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+
+<html lang=en-US>
+<head><meta content="text/html;charset=UTF-8" http-equiv=Content-type>
+
+    <title>Hello World</title>
+    <!-- css stolen from w3c -->
+    <link type="text/css" href="/default.css" rel=stylesheet>
+    <link type="text/css" href="/base.css" rel=stylesheet>
+
+<body>
+<div class=head>
+    <h1 id=media-queries>Hello World</h1>
+</div>
+<p>Minimal static HTML page</p>
+</body>
+</html>

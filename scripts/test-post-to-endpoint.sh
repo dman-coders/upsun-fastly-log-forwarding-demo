6d9add36c717be8a1d0dc4ce2b35754f6a25b980
dman <dman@coders.co.nz>
2025-12-25 17:39:53 +0700

    first cut at log_reciever

---
+++ scripts/test-post-to-endpoint.sh
@@ -0,0 +1,22 @@
+#!/bin/env bash
+
+# Make a dummy POST to our log consumer.
+
+BASE_URL=$(upsun environment:url --primary --pipe)
+ENDPOINT_URL="${BASE_URL}log-receiver.php"
+
+echo "Posting to endpoint URL: ${ENDPOINT_URL}"
+
+curl -X POST \
+  -H "Content-Type: application/json" \
+  -d '{
+    "level": "info",
+    "message": "This is a test log message from the test-post-to-endpoint.sh script.",
+    "context": {
+        "user": "test_user",
+        "action": "test_post"
+    },
+    "timestamp": "'$(date -u +"%Y-%m-%dT%H:%M:%SZ")'"
+}' \
+  "${ENDPOINT_URL}"
+

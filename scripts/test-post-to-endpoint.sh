338caee4298af308cdb94d5914af56532a1b7033
dman <dman@coders.co.nz>
2025-12-26 14:32:56 +0700

    add a reports directory and publish it

--- scripts/test-post-to-endpoint.sh
+++ scripts/test-post-to-endpoint.sh
@@ -5,17 +5,8 @@ LOG_RECEIVER_URL=$(upsun environment:url --primary --pipe)
 ENDPOINT_URL="${LOG_RECEIVER_URL}log-receiver.php"
 
 echo "Posting to endpoint URL: ${ENDPOINT_URL}"
-
+LOG_LINE="1.2.3.4 - - $(date -u +"[%d/%b/%Y:%H:%M:%S +0000]") \"GET /dummy.html HTTP/1.1\" 200 43 \"-\" \"Log receiver tester\""
 curl -X POST \
-  -H "Content-Type: application/json" \
-  -d '{
-    "level": "info",
-    "message": "This is a test log message from the test-post-to-endpoint.sh script.",
-    "context": {
-        "user": "test_user",
-        "action": "test_post"
-    },
-    "timestamp": "'$(date -u +"%Y-%m-%dT%H:%M:%SZ")'"
-}' \
+  -H "Content-Type: text/plain" \
+  --data-binary "${LOG_LINE}" \
   "${ENDPOINT_URL}"
-

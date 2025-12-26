22a758e6642a3d744bde3a0afd2401ab68663a93
dman <dman@coders.co.nz>
2025-12-26 16:13:31 +0700

    trigger a rebuild from the landing page

--- web/index.php
+++ web/index.php
@@ -12,6 +12,18 @@
 <div class=head>
     <h1 id=media-queries>Hello World</h1>
 </div>
-<p>Minimal static HTML page</p>
+<p>
+  <a href="reports/index.html">Fastly traffic report</a>
+</p>
+<img
+  style='width: 100px; height: 100px;
+      position: relative;
+      background: url("data:image/svg+xml,%3Csvg xmlns=%27http://www.w3.org/2000/svg%27 viewBox=%270 0 100 100%27%3E%3Ccircle cx=%2750%27 cy=%2750%27 r=%2745%27 fill=%27%233b82f6%27 stroke=%27%232563eb%27 stroke-width=%273%27/%3E%3Cpath d=%27M 50 25 A 20 20 0 1 1 30 45%27 stroke=%27white%27 stroke-width=%276%27 stroke-linecap=%27round%27 fill=%27none%27/%3E%3Cpath d=%27M 50 20 L 50 30 L 40 25 Z%27 fill=%27white%27/%3E%3C/svg%3E") center no-repeat;
+      background-size: contain;'
+  src="generate-report.php"
+  alt="Generating report"
+/>
+
+
 </body>
 </html>
